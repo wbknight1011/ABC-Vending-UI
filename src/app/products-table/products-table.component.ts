49d6513013b70620be3a416be228414e62048bc7
Blake Knight <wbknight.william@gmail.com>
2022-10-29 16:06:57 -0400

    add column sorting to products

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-products-table',
   templateUrl: './products-table.component.html',
   styleUrls: ['./products-table.component.scss']
 })
-export class ProductsTableComponent implements OnInit {
+export class ProductsTableComponent implements AfterViewInit {
+  productSource = new MatTableDataSource<Product>(hardCodedProducts);  
+  displayedColumns : string [] = ['name', 'warehouseName', 'category', 'cost', 'stock'];
+  
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
-  constructor() { }
+  @ViewChild(MatSort) sort!: MatSort;  
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.productSource.sort = this.sort;
   }
 
+  announceSortChange(sortState: Sort) 
+  {
+    if (sortState.direction) 
+    {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } 
+    else 
+    {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
 
 export interface Product
@@ -19,7 +38,20 @@ export interface Product
   id: string;
   vendingMachineId: string;
   warehouseId: string;
+  warehouseName: string;
   name: string;
   category: string;
   cost: number;
+  stock: number;
 }
+
+const hardCodedProducts : Product[] = [
+  { id: '001', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Pepsi', category: 'Soda', cost: 1.25, stock: 1255 },
+  { id: '002', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Coke', category: 'Soda', cost: 1.25, stock: 850 },
+  { id: '003', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Dr. Pepper', category: 'Soda', cost: 1.25, stock: 715 },
+  { id: '004', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Mountain Dew', category: 'Soda', cost: 1.25, stock: 225 },
+  { id: '005', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Doritos', category: 'Chips', cost: 1.25, stock: 400 },
+  { id: '006', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Cheetos', category: 'Chips', cost: 1.25, stock: 568 },
+  { id: '007', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Honey Bun', category: 'Confectionary', cost: 1.25, stock: 745 },
+  { id: '008', vendingMachineId: '001', warehouseId: '001', warehouseName: 'First Opened Warehouse LLC', name: 'Powdered Donuts', category: 'Confectionary', cost: 1.25, stock: 385 },
+]
